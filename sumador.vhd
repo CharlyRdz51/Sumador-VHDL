4ab70adc39c290d4865d182d2be1de4e23d68d47
CharlyRdz <51723784+CharlyRdz51@users.noreply.github.com>
2022-11-02 01:00:34 -0600

    Add files via upload

---
+++ sumador.vhd
@@ -0,0 +1,56 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    11:53:32 10/06/2016 
+-- Design Name: 
+-- Module Name:    sumador - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity sumador is
+    Port ( reloj : in  STD_LOGIC;
+           reset : in  STD_LOGIC;
+           salida : out  STD_LOGIC_VECTOR (3 downto 0));
+end sumador;
+
+architecture Behavioral of sumador is
+signal registro : std_logic_vector (3 downto 0);
+begin
+
+
+process(reloj,reset)
+begin
+if reset = '1' then 
+registro<= "0000";
+else
+if
+reloj'event and reloj = '1' then
+registro<=registro + '1';
+end if;
+end if;
+end process;
+end Behavioral;
+
